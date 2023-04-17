af7d2f1ed3dd12a1ee9b4f923c1b168323f1b6a4
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-04-16 21:47:31 -0600

    updt

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,11 @@
-export const API_KEY = process.env.REACT_APP_API_KEY as string;
-export const THE_CAT_API = process.env.REACT_APP_THE_CAT_API as string;
-export const PICS_API = process.env.REACT_APP_PICS_API as string;
+// REACT_APP_API_KEY=live_hxxOWYd7klK0ePD7u7i6r0NggLZRbNNUqa13qIC1TKTMwbDllaQ8swPiUqk5oP99
+// REACT_APP_THE_CAT_API=https://api.thecatapi.com/v1/breeds/
+// REACT_APP_PICS_API=https://api.thecatapi.com/v1/images/search?&api_key=
+
+
+
+export const API_KEY = 'live_hxxOWYd7klK0ePD7u7i6r0NggLZRbNNUqa13qIC1TKTMwbDllaQ8swPiUqk5oP99' as string;
+export const THE_CAT_API = 'https://api.thecatapi.com/v1/breeds/' as string;
+export const PICS_API = 'https://api.thecatapi.com/v1/images/search?&api_key=' as string;
+
+
